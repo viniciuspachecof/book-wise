d11f62992ae9d640d99f47cfe3677a08ac8ad195
Vinicius Pacheco <vinicius.pachecoof@hotmail.com>
2025-09-17 21:50:07 -0300

    Persistir usuário (Prisma e Next Auth)

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -1,27 +1,59 @@
 import NextAuth, { NextAuthOptions } from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
+import GoogleProvider, { GoogleProfile } from 'next-auth/providers/google';
+import GitHubProvider, { GithubProfile } from 'next-auth/providers/github';
+import { PrismaAdapter } from '@/lib/auth/prisma.adapter';
+import { NextApiRequest, NextApiResponse, NextPageContext } from 'next';
 
-export const authOptions: NextAuthOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID ?? '',
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
-      authorization: {
-        params: {
-          prompt: 'consent',
-          access_type: 'offline',
-          response_type: 'code',
-          scope: 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile',
+export function buildNextAuthOptions(
+  req: NextApiRequest | NextPageContext['req'],
+  res: NextApiResponse | NextPageContext['res']
+): NextAuthOptions {
+  return {
+    adapter: PrismaAdapter(req, res),
+
+    providers: [
+      GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+        authorization: {
+          params: {
+            prompt: 'consent',
+            access_type: 'offline',
+            response_type: 'code',
+            scope: 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile',
+          },
         },
+        profile(profile: GoogleProfile) {
+          return {
+            id: profile.sub,
+            name: profile.name,
+            email: profile.email,
+            avatar_url: profile.picture,
+          };
+        },
+      }),
+      GitHubProvider({
+        clientId: process.env.GITHUB_ID ?? '',
+        clientSecret: process.env.GITHUB_SECRET ?? '',
+        profile(profile: GithubProfile) {
+          return {
+            id: profile.id.toString(),
+            name: profile.name || profile.login,
+            email: profile.email ?? '',
+            avatar_url: profile.avatar_url,
+          };
+        },
+      }),
+    ],
+
+    callbacks: {
+      async redirect({ url, baseUrl }) {
+        return `${baseUrl}/`;
       },
-    }),
-  ],
-
-  callbacks: {
-    async redirect({ url, baseUrl }) {
-      return `${baseUrl}/`;
     },
-  },
-};
+  };
+}
 
-export default NextAuth(authOptions);
+export default async function auth(req: NextApiRequest, res: NextApiResponse) {
+  return await NextAuth(req, res, buildNextAuthOptions(req, res));
+}
