af60783a46fdacf71476850fdff44cc79a39d260
Vinicius Pacheco <vinicius.pachecoof@hotmail.com>
2026-04-22 23:42:57 -0300

    Adicionado titulo aba

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -5,6 +5,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <title>Book Wise</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
